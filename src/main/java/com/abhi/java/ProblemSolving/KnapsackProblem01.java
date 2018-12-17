c8bf2c77351d9ec9392a2bcc7e9ad2c958872bd8
abhinav-143628 <abhinav.dogra@sapient.com>
2018-12-17 17:03:46 +0530

    test

---
+++ src/main/java/com/abhi/java/ProblemSolving/KnapsackProblem01.java
@@ -0,0 +1,41 @@
+package com.abhi.java.ProblemSolving;
+
+/**
+ * Created by abhdogra1 on 12/17/2018.
+ */
+public class KnapsackProblem01 {
+
+    public static void main(String[] args) {
+        int[] value = {60, 100, 120};
+        int[] weight = {10, 20, 30};
+        int weightRequired = 50;
+        int length = value.length;
+        System.out.println(knapsack(value, weight, weightRequired, length));
+    }
+
+    private static int knapsack(int[] value, int[] weight, int weightRequired, int length) {
+
+        if (value[length - 1] == 0 || weight[length - 1] == 0)
+            return 0;
+
+        if (weight[length - 1] > weightRequired)
+            knapsack(value, weight, weightRequired, length - 1);
+        else
+            return Math.max(knapsack(value, weight, weightRequired, length - 1),
+                    knapsack(value, weight, weightRequired, length - 1));
+
+//        int maxValue =0, maxWeight = 0;
+//
+//        for(int i=0;i<length;i++){
+//            maxWeight +=weight[i];
+//            if(maxWeight > weightRequired)
+//                break;
+//            else if(maxWeight <=weightRequired){
+//                maxValue += value[i];
+//            }
+//        }
+        return maxValue;
+    }
+}
+
+
---
+++ src/main/java/com/abhi/java/threads/ArraysIndexAdditionThreadPool/ThreadsMainClass.java
@@ -0,0 +1,24 @@
+package com.abhi.java.threads.ArraysIndexAdditionThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by abhdogra1 on 12/17/2018.
+ */
+public class ThreadsMainClass {
+
+    int[] arr = {};
+    int[] arr2 = {};
+
+    int[] resultant;
+
+    public void addIndexesofArray(){
+
+    }
+
+    public static void main(String[] args) {
+        ExecutorService ex = Executors.newFixedThreadPool(5);
+
+    }
+}
