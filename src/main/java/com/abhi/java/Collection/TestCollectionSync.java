fd966f405c93695389b363a293e8241fb00f6a11
abhinav-143628 <abhinav.dogra@sapient.com>
2018-12-19 16:31:51 +0530

    Update TestCollectionSync.java

--- src/main/java/com/abhi/java/Collection/TestCollectionSync.java
+++ src/main/java/com/abhi/java/Collection/TestCollectionSync.java
@@ -1,5 +1,6 @@
 package com.abhi.java.Collection;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -10,9 +11,9 @@ import java.util.Hashtable;
 public class TestCollectionSync {
 
     public static void main(String[] args) {
-        //Difference betweeen below 2
-        Collections.synchronizedMap(new HashMap<String,Integer>());
-        Hashtable<String,Integer> tableMap = new Hashtable<>();
-
+        //Difference betweeen below collections
+        Collections.synchronizedMap(new HashMap<String,Integer>()); // in this we can provide own mutex for locks
+        Hashtable<String,Integer> tableMap = new Hashtable<>(); // in this we cannot, we all methods are sync
+        Collections.unmodifiableList(new ArrayList<>()); // retruns intance of UnmodifiableList or UnmodifiableRandomAccessList in which they return UnsupportedOperationException for modifying operations except get, iterator and all
     }
 }
