04a52ed14d0bc01f9d80a43b7d575962af4ca5e4
abhinav-143628 <abhinav.dogra@sapient.com>
2018-12-27 09:45:48 +0530

    work done before vacation

--- src/main/java/com/abhi/java/Collection/TestCollectionSync.java
+++ src/main/java/com/abhi/java/Collection/TestCollectionSync.java
@@ -1,9 +1,6 @@
 package com.abhi.java.Collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Hashtable;
+import java.util.*;
 
 /**
  * Created by abhdogra1 on 12/13/2018.
@@ -14,6 +11,8 @@ public class TestCollectionSync {
         //Difference betweeen below collections
         Collections.synchronizedMap(new HashMap<String,Integer>()); // in this we can provide own mutex for locks
         Hashtable<String,Integer> tableMap = new Hashtable<>(); // in this we cannot, we all methods are sync
-        Collections.unmodifiableList(new ArrayList<>()); // retruns intance of UnmodifiableList or UnmodifiableRandomAccessList in which they return UnsupportedOperationException for modifying operations except get, iterator and all
+        Collections.unmodifiableList(new ArrayList<>()); // returns instance of UnmodifiableList or UnmodifiableRandomAccessList in which they return UnsupportedOperationException for modifying operations except get, iterator and all
+        TreeMap<String, Integer> map;
+        TreeSet<String> mapSet;
     }
 }
---
+++ src/main/java/com/abhi/java/JUnitActualClasses/ExceptionCheckClass.java
@@ -0,0 +1,12 @@
+package com.abhi.java.JUnitActualClasses;
+
+/**
+ * Created by abhdogra1 on 12/20/2018.
+ */
+public class ExceptionCheckClass {
+
+    public void generateExceptions(){
+
+    }
+
+}
