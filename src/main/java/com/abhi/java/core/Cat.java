11eb8e45f57f7528cebf5f60c295ee8f76dc805f
abhinav-143628 <abhinav.dogra@sapient.com>
2019-01-09 18:01:52 +0530

    problem solving

---
+++ src/main/java/com/abhi/java/core/Cat.java
@@ -0,0 +1,12 @@
+package com.abhi.java.core;
+
+/**
+ * Created by abhdogra1 on 1/8/2019.
+ */
+public class Cat extends Animal {
+    public Cat eat(){
+        System.out.println("cat");
+        return this;
+    }
+}
+
