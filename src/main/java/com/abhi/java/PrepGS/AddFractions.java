e5685393e1910bfaab3e21f7ebf79e38d9d9df86
abhinav-143628 <abhinav.dogra@sapient.com>
2019-02-01 17:42:12 +0530

    Update AddFractions.java

--- src/main/java/com/abhi/java/PrepGS/AddFractions.java
+++ src/main/java/com/abhi/java/PrepGS/AddFractions.java
@@ -10,18 +10,60 @@ public class AddFractions {
      * Returns true if all the tests pass. Otherwise returns false.
      */
     public static boolean doTestsPass() {
-        // TODO: implement some tests, please
-        // we've included a trivial boilerplate
 
-        int[] result = addFractions( new int[]{ 2, 3 }, new int[]{ 1, 2 } );
+        boolean testPassed = true;
+        int[] result;
+        int[] fraction1 = { 2, 3 };
+        int[] fraction2 = { 1, 2 };
 
-        if( result[ 0 ] == 7 && result[ 1 ] == 6 ) {
-            System.out.println( "Test passed." );
-            return true;
-        } else {
-            System.out.println( "Test failed." );
-            return false;
+        System.out.println( "Running Test #1" );
+        result = addFractions( fraction1, fraction2 );
+
+        if( result[ 0 ] != 7 || result[ 1 ] != 6 ) {
+            System.out.println( "Test #1 failed" );
+            testPassed = false;
         }
+
+        System.out.println( "Running Test #2" );
+        fraction1[ 0 ] = 5;
+        fraction1[ 1 ] = 6;
+        fraction2[ 0 ] = 2;
+        fraction2[ 1 ] = 3;
+        result = addFractions( fraction1, fraction2 );
+        if( !(result[ 0 ] == 3 && result[ 1 ] == 2) ) {
+            System.out.println( "Test #2 failed" );
+            testPassed = false;
+        }
+
+        System.out.println( "Running Test #3" );
+        fraction2[ 1 ] = 0;
+        try {
+            result = addFractions( fraction1, fraction2 );
+            //reaching here means that exception was not thrown indicating that the test failed
+            System.out.println( "Test #3 failed" );
+            testPassed = false;
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        System.out.println( "Running Test #4" );
+        fraction1 = new int[ 1 ];
+        try {
+            result = addFractions( fraction1, fraction2 );
+            //reaching here means that exception was not thrown indicating that the test failed
+            System.out.println( "Test #4 failed" );
+            testPassed = false;
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        if( testPassed ) {
+            System.out.println( "All tests pass." );
+        } else {
+            System.out.println( "There are test failures." );
+        }
+
+        return testPassed;
     }
 
     public static void main( String[] args ) {
@@ -29,57 +71,71 @@ public class AddFractions {
     }
 
     private static int[] addFractions(int[] a, int[] b) {
+
+        if(a.length != 2 || b.length != 2){
+            throw new IllegalArgumentException( "Arguments passed should be two-element arrays"  );
+        }
+
         int num1 = a[0];
         int num2 = b[0];
 
         int dem1 = a[1];
         int dem2 = b[1];
 
+        if(dem1 == 0 || dem2 == 0)
+            throw new IllegalArgumentException( "Denominator in at least one of the input fractions is zero, which is not allowed." );
+
         int[] sumRes = new int[2];
 
         int numerator = num1*dem2 + num2*dem1;
         int denomerator = dem2*dem1;
 
-        sumRes[0] = numerator;
-        sumRes[1] = denomerator;
+        if(numerator == 0)
+            return (new int[]{0,1});
 
-        sumRes = findGCD(sumRes);
+       // int divisor = findGCD(numerator, denomerator);
+        int divisor = findGCDLiner(numerator,denomerator);
 
-        //findGCDReccur(sumRes,0);
-        
-        return sumRes;
+        if(divisor == 0)
+            return sumRes;
+        else{
+            sumRes[0] = numerator/divisor;
+            sumRes[1] = denomerator/divisor;
+            return sumRes;
+        }
     }
 
-//    private static int findGCDReccur(int[] sumRes, int divior) {
-//        if(sumRes[0] > sumRes[1]){
-//            divior = sumRes[1]/sumRes[0];
-//        }else{
-//            divior =  sumRes[0]/sumRes[1];
-//        }
-//
-//        findGCDReccur(sumRes,divior);
-//    }
+    /*
+    * Using Euclid's algorithm
+    * gcd(a,0)=a  //return condition
+    * gcd(a,b) = gcd(b, a mod b); //recursively call this till above condition is matched
+    * */
+    private static int findGCD(int num, int dem) {
+        if(dem == 0)
+            return num;
+        return findGCD(dem, num % dem);
 
-    private static int[] findGCD(int[] sumRes) {
-        int num = sumRes[0];
-        int dum = sumRes[1];
-        int divisor;
-        int mod;
+    }
 
-        if(num<dum){
-            mod = dum % num;
-            divisor = num%mod;
-        }else{
-            mod = num%dum;
-            divisor = dum%mod;
-        }
+    /*
+    * We find the min value and from both the fractions and take it as gcd
+    * we check if at any given value of gcd if mod of fractions is 0 with given value of gcd then we break and it will be our required gcd
+    * for gcd value 1, given condition will pass in case we are not able to find a common divisor
+    * we are starting from min value and decrementing gcd value because we want to find the HIGHEST COMMON DIVISOR for both
+    *
+    * */
+    private static int findGCDLiner(int num, int dem) {
+        int gcd = Math.min(Math.abs(num),Math.abs(dem));
 
-        if(divisor == 0){
-            sumRes[0] = num/mod;
-            sumRes[1] = dum/mod;
-        }
+        if(gcd != 0){
+            while(gcd !=0){
+                if(num % gcd ==0 && dem % gcd == 0)
+                    break;
+                gcd--;
+            }
+        }else
+            gcd = 1;
 
-        return sumRes;
-
+        return gcd;
     }
 }
