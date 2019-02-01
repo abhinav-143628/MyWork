464e50808569193527d6c6aa0f0ef06cb5598d81
abhinav-143628 <abhinav.dogra@sapient.com>
2019-02-01 19:44:41 +0530

    Update TrainMap.java

--- src/main/java/com/abhi/java/PrepGS/TrainMap.java
+++ src/main/java/com/abhi/java/PrepGS/TrainMap.java
@@ -111,11 +111,76 @@ public class TrainMap {
         }
 
         public List<Station> shortestPath(String from, String to) {
-    /*
-     * TODO Implement
-     * 0
-     */
-            return Collections.emptyList();
+        /*
+         * TODO Implement
+         */
+            Station fromStation = stations.get(from);
+            Station toStation = stations.get(to);
+
+            return getShortestPath(fromStation, toStation);
+        }
+
+
+        private List<Station> getShortestPath(Station fromStation, Station toStation) {
+
+            //this queue will keep on adding elemets to it and will poll then mark them as visited
+            Queue<Station> isVisited = new LinkedList<Station>();
+            //this map is used to taking the path with us, we store every node as key and its previous node as its value
+            //we will use this map to generate the final path
+            // this map is like Map<currentPostion, parentPositionOfCurrent>,
+            // parentPositionOfCurrent is position form where current is getting callled or linked to.
+            HashMap<Station, Station> pathMapping = new HashMap<>();
+
+            //we add start point to the queue
+            isVisited.add(fromStation);
+            //as from start point there will be no parent to
+            pathMapping.put(fromStation,null);
+
+            //We traverse the queue till it is empty
+            while(!isVisited.isEmpty()){
+                //poll the firt station
+                Station st = isVisited.poll();
+                //if first station is our destination then there is not path then path is null
+                if(st.getName().equalsIgnoreCase(toStation.getName())){
+                    break;
+                }
+                // for polled station, we check for every node it points to add it to queue
+                // and update pathMapping map will child node with its parent node which is polled node from the queue
+                // which we polled above
+                // here we also have to make visited check,
+                // for visited node check, we take help of map, we are adding every nodes to map,
+                // if that node is already present in the map, then that means we have already visited that node
+                for(Station neighbour : st.getNeighbours()){
+                    if(!pathMapping.containsKey(neighbour)){
+                        isVisited.add(neighbour);
+                        pathMapping.put(neighbour,st);
+                    }
+                }
+            }
+
+            //after above while loop, we know that either we came out from break statement
+            // or we traversed entire graph and destination node was not found.
+            // we make that check here
+            if(!pathMapping.containsKey(toStation))
+                return null;
+
+            // resultant list to hold the path
+            List<Station> result = new LinkedList<>();
+
+            Station curr = toStation;
+
+            // we start from destination and traverse back in the map to its source station, which will return the value null
+            // as above we inserted source value as nulll (as there is no parent for source)
+            while (curr != null){
+                result.add(0,curr);
+                curr = pathMapping.get(curr);
+            }
+
+            //"King's Cross St Pancras->Russel Square->Holborn->Chancery Lane->St Paul's";
+
+            return result;
+
+
         }
 
         public static String convertPathToStringRepresentation(List<Station> path) {
